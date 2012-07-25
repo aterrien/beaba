ca8251236961e14e2ff5d4820ae833853136f977
Ioan CHIRIAC <i.chiriac@yahoo.com>
2012-07-25 23:38:42 +0200

    handle routing errors (file & class not found)

--- framework/config/routes.php
+++ framework/config/routes.php
@@ -15,7 +15,27 @@ return array(
         'route' => function( $url ) {
             $parts = explode('/', $url, 2);
             if ( !isset( $parts[2] ) ) $parts[2] = 'index';
-            return 'application\\controllers\\' . strtolower($parts[1]) . '::' . strtolower($parts[2]);
+            $target = BEABA_APP . '/controllers/' . strtolower($parts[1]) . 'php';
+            $class = 'application\\controllers\\' . strtolower($parts[1]);
+            if ( !class_exists( $class, false ) ) {
+                if ( 
+                    !file_exists( $target ) 
+                ) {
+                    throw new \beaba\core\Exception(
+                        'Unable to find controller : ' . strtolower($parts[1]), 
+                        404
+                    );
+                } else {
+                    include $target;
+                    if ( !class_exists( $class, false ) ) {
+                        throw new \beaba\core\Exception(
+                            'Unable to find class controller : ' . $class, 
+                            404
+                        );
+                    }
+                }                
+            }
+            return $class . '::' . strtolower($parts[2]);
         }
     ),
     'beaba' => array(
