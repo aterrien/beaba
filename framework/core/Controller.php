a9ac14a1aea005fad13a71723d04624b1f52b345
Ioan CHIRIAC <i.chiriac@yahoo.com>
2012-07-25 22:10:33 +0200

    fix code style

--- framework/core/Controller.php
+++ framework/core/Controller.php
@@ -5,21 +5,24 @@ namespace beaba\core;
  * License. See README.MD for details.
  * @author Ioan CHIRIAC
  */
-class Controller {
-    protected $app;
+class Controller 
+{
+    protected $_app;
     /**
      * Initialize a new controller with the specified app
      * @param Application $app 
      */
-    public function __construct( Application $app ) {
-        $this->app = $app;
+    public function __construct( Application $app ) 
+    {
+        $this->_app = $app;
     }
     /**
      * Executes the specified action
      * @param string $action
      * @param array $params 
      */
-    public function execute( $action, $params ) {        
+    public function execute( $action, $params ) 
+    {        
         if ( !is_callable( array( $this, $action ) ) ) {
             throw new Exception(
                 'Undefined action : ' . $action, 501
