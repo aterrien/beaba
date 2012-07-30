de3220111f825e8f910d94d710f6cb03dea9963d
Ioan CHIRIAC <i.chiriac@yahoo.com>
2012-07-30 23:16:18 +0200

    can now pass a function to dispatch

--- framework/core/Application.php
+++ framework/core/Application.php
@@ -1,62 +1,61 @@
 <?php
+
 namespace beaba\core;
+
 /**
  * This file is distributed under the MIT Open Source
  * License. See README.MD for details.
  * @author Ioan CHIRIAC
  */
 class Application extends Event
-{    
-    
+{
     /**
      * the application start
      */
-    const E_LOAD        = 'onLoad';
+    const E_LOAD = 'onLoad';
     /**
      * before the request dispatch
      */
-    const E_DISPATCH    = 'onDispatch';
+    const E_DISPATCH = 'onDispatch';
     /**
      * if an error occurs during the dispatch
      */
-    const E_ERROR       = 'onError';
+    const E_ERROR = 'onError';
     /**
      * before rendering the view
      */
-    const E_BEFORE_RENDER      = 'beforeRender';
+    const E_BEFORE_RENDER = 'beforeRender';
     /**
      * before rendering the view
      */
-    const E_AFTER_RENDER      = 'afterRender';
+    const E_AFTER_RENDER = 'afterRender';
     /**
      * the application ends
      */
-    const E_UNLOAD      = 'onUnload';
+    const E_UNLOAD = 'onUnload';
     /**
      * @var array List of services configuration
      */
     protected $_services;
+
     /**
      * @var array List of services instances
      */
     protected $_instances = array();
+
     /**
      * The current configuration instance
      * @var Configuration
      */
     public $config;
-    /**
-     * The website base dir
-     * @var string 
-     */
-    public $base_dir;
+
     /**
      * Initialize the application
      */
-    public function __construct( array $config = null ) 
+    public function __construct(array $config = null)
     {
         // initialize the configuration layer
-        $this->config = new Configuration( $config );
+        $this->config = new Configuration($config);
         // initialize the event handler
         parent::__construct($this);
         // attach core default services : error manager + logger
@@ -64,75 +63,92 @@ class Application extends Event
             $this->getService('logger')
         );
         // raise the application start event
-        $this->_raise( self::E_LOAD );
+        $this->_raise(self::E_LOAD);
     }
+
     /**
      * Uninitialize the app
      */
-    public function __destruct() 
+    public function __destruct()
     {
-        $this->_raise( self::E_UNLOAD );
+        $this->_raise(self::E_UNLOAD);
     }
+
     /**
      * Gets a service instance
      * @param string $name 
      * @return IService
      */
-    public function getService( $name ) 
+    public function getService($name)
     {
-        if ( !isset( $this->_instances[ $name ] ) ) {
+        if (!isset($this->_instances[$name])) {
             if (!$this->_services) {
                 $this->_services = $this->config->getConfig('services');
             }
-            if ( !isset( $this->_services[ $name ]) ) {
+            if (!isset($this->_services[$name])) {
                 throw new \Exception(
                     'Undefined service : ' . $name
                 );
             }
-            $this->_instances[ $name ] = new $this->_services[ $name ]( $this );
+            $this->_instances[$name] = new $this->_services[$name]($this);
         }
-        return $this->_instances[ $name ];
-    }   
+        return $this->_instances[$name];
+    }
+
     /**
      * Gets the informations layer
      * @return IInfos
      */
-    public function getInfos() 
+    public function getInfos()
     {
         return $this->getService('infos');
     }
+
     /**
      * Gets the asset manager
      * @return IAssets
      */
-    public function getAssets() 
+    public function getAssets()
     {
         return $this->getService('assets');
     }
+
     /**
      * Gets the response handler
      * @return IResponse
      */
-    public function getResponse() 
+    public function getResponse()
     {
         return $this->getService('response');
     }
+
     /**
      * Gets the view manager
      * @return IView
      */
-    public function getView() 
+    public function getView()
     {
         return $this->getService('view');
     }
+
     /**
      * Gets the logging service
      * @return ILogger
      */
-    public function getLogger() 
+    public function getLogger()
     {
         return $this->getService('logger');
     }
+
+    /**
+     * Gets the current request
+     * @return IRequest
+     */
+    public function getRequest()
+    {
+        return $this->getService('request');
+    }
+
     /**
      * Execute the specified action controller 
      * @param string $controller
@@ -140,397 +156,511 @@ class Application extends Event
      * @param array $params 
      * @return string
      */
-    public function execute( $controller, $action, $params ) 
+    public function execute($controller, $action, $params)
     {
-        $instance = new $controller( $this );
-        return $instance->execute( $action, $params );
-    }    
+        $instance = new $controller($this);
+        return $instance->execute($action, $params);
+    }
+
     /**
      * Dispatching the specified request
      * @param string $url
      * @param array $params 
      */
-    public function dispatch( $url, $params ) 
-    {        
+    public function dispatch($url = null, array $params = null)
+    {
         try {
-            $this->_raise( 
-                self::E_DISPATCH, 
-                array(
-                    'request'   => $url,
-                    'params'    => $params
-                ) 
+            $params = $params ?
+                merge_array(
+                    $this->getRequest()->getParameters(), $params
+                ) :
+                $this->getRequest()->getParameters()
+            ;
+            $this->_raise(
+                self::E_DISPATCH, array(
+                'request' => $url,
+                'params' => $params
+                )
             );
-            $this->base_dir = substr(
-                $_SERVER['SCRIPT_NAME'], 0, 
-                strrpos($_SERVER['SCRIPT_NAME'], '/')
-            );
-            $query = strpos($url, '?');
-            $url = substr(
-                $url, strlen($this->base_dir), 
-                $query !== false ? 
-                    $query - strlen($this->base_dir) : strlen($url)
-            );
-            $route = $this->getService('router')->getRoute( $url );
-            if ( $route === false ) {
+            if (!is_callable($url)) {
+                // initialize parameters
+                if (!is_null($url)) {
+                    $this->getRequest()->setLocation($url);
+                } else {
+                    $url = $this->getRequest()->getLocation();
+                }
+                $route = $this->getService('router')->getRoute($url);
+            } else {
+                $route = $url;
+            }
+            if ($route === false) {
                 throw new Exception('No route found', 404);
             } else {
-                if ( is_string( $route ) ) {
+                if (is_string($route)) {
                     // execute a controller
-                    $parts = explode( '::', $route, 2 );
-                    if ( empty($parts[1]) ) $parts[1] = 'index';
-                    $this->execute( $parts[0], $parts[1], $params );
+                    $parts = explode('::', $route, 2);
+                    if (empty($parts[1]))
+                        $parts[1] = 'index';
+                    $this->execute($parts[0], $parts[1], $params);
                 } else {
                     // use the route as a callback
-                    $route( $this, $params );                                         
+                    $route($this, $params);
                 }
             }
-        } catch( \Exception $ex ) {
-            $this->_raise( 
-                self::E_ERROR, 
-                array(
-                    'request'   => $url,
-                    'params'    => $params,
-                    'error'     => $ex
-                ) 
-            );            
-            if ( $ex instanceof Exception && !$ex->isHttpError() ) {
+        } catch (\Exception $ex) {
+            $this->_raise(
+                self::E_ERROR, array(
+                'request' => $url,
+                'params' => $params,
+                'error' => $ex
+                )
+            );
+            if ($ex instanceof Exception && !$ex->isHttpError()) {
                 $this->getService('response')->setCode(
                     $ex->getCode(), $ex->getHttpMessage()
-                );                
+                );
             } else {
                 $this->execute(
-                    'beaba\\controllers\\errors', 'show', 
-                    array(
-                        'request' => $url,
-                        'params' => $params,
-                        'error' => $ex
+                    'beaba\\controllers\\errors', 'show', array(
+                    'request' => $url,
+                    'params' => $params,
+                    'error' => $ex
                     )
-                );                
+                );
             }
         }
-        $this->_raise( self::E_BEFORE_RENDER );
+        $this->_raise(self::E_BEFORE_RENDER);
         $response = $this->getView()->renderTemplate();
-        $this->_raise( 
-            self::E_AFTER_RENDER, 
-            array(
-                'response' => &$response
+        $this->_raise(
+            self::E_AFTER_RENDER, array(
+            'response' => &$response
             )
         );
-        $this->getResponse()->write( $response );        
+        $this->getResponse()->write($response);
     }
+
 }
+
 /**
  * The service interface
  */
-interface IService 
+interface IService
 {
+
     /**
      * Gets the current application
      * @return Application
      */
     function getApplication();
 }
+
 /**
  * 
  */
-interface IPlugins extends IService 
+interface IPlugins extends IService
 {
     
 }
 
 /**
+ * Defines the requesting service
+ */
+interface IRequest extends IService
+{
+
+    /**
+     * Gets the requested method type
+     * @see GET, POST, PUT, DELETE ...
+     * @return string
+     */
+    public function getMethod();
+
+    /**
+     * Gets the requested unique ressource location
+     * @see /index.html
+     * @return string
+     */
+    public function getLocation();
+
+    /**
+     * Sets the requested unique ressource location
+     * @params string $url
+     * @return IRequest
+     */
+    public function setLocation($url);
+
+    /**
+     * Gets the response type : html, xml, json ...
+     * @return string
+     */
+    public function getResponseType();
+
+    /**
+     * Gets the list of requested parameters
+     * @return array
+     */
+    public function getParameters();
+
+    /**
+     * Gets the specified parameter
+     * @return mixed
+     */
+    public function getParameter($name);
+
+    /**
+     * Check if the specified parameter is defined
+     * @return boolean
+     */
+    public function hasParameter($name);
+}
+
+/**
  * The informations layer
  */
-interface IInfos extends IService 
+interface IInfos extends IService
 {
+
     /**
      * Check if the specified configuration key is defined
      * @param string $key
      * @return boolean 
      */
-    public function hasConfig( $key );
+    public function hasConfig($key);
+
     /**
      * Gets the configuration entry
      * @param string $key 
      * @return mixed
      */
-    public function getConfig( $key );
+    public function getConfig($key);
+
     /**
      * Sets the specified configuration entry
      * @param string $key
      * @param mixed $value 
      * @return void
      */
-    public function setConfig( $key, $value );
+    public function setConfig($key, $value);
+
     /**
      * Gets the application name
      * @return string
      */
     public function getName();
+
     /**
      * Sets the current application name
      * @param string $value 
      */
-    public function setName( $value );
+    public function setName($value);
+
     /**
      * Gets the page title
      * @return string
      */
     public function getTitle();
+
     /**
      * Sets the current page title
      * @param string $value 
      */
-    public function setTitle( $value );
+    public function setTitle($value);
+
     /**
      * Gets the page description
      * @return string
      */
     public function getDescription();
+
     /**
      * Sets the current page description
      * @param string $value 
      */
-    public function setDescription( $value );
+    public function setDescription($value);
+
     /**
      * Gets the page template
      * @return string
      */
     public function getTemplate();
+
     /**
      * Sets the current page template
      * @param string $value 
      */
-    public function setTemplate( $value );
+    public function setTemplate($value);
+
     /**
      * Gets the page layout
      * @return string
      */
     public function getLayout();
+
     /**
      * Sets the current page layout
      * @param string $value 
      */
-    public function setLayout( $value );
+    public function setLayout($value);
 }
+
 /**
  * The router interface
  */
-interface IRouter extends IService 
+interface IRouter extends IService
 {
-/**
+
+    /**
      * Retrieves a list of routes from the configuration
      * @return array
      */
-    public function getRoutes();    
+    public function getRoutes();
+
     /**
      * Gets the requested route
      * @param string $url
      * @return string 
      */
-    public function getRoute( $url );    
+    public function getRoute($url);
 }
+
 /**
  * The view interface
  */
-interface IView extends IService 
+interface IView extends IService
 {
+
     /**
      * Sets the main layout 
      * @return IView
      */
-    public function setLayout( $file );
+    public function setLayout($file);
+
     /**
      * Sets the templating file
      * @return IView
      */
-    public function setTemplate( $file );
+    public function setTemplate($file);
+
     /**
      * Adds the specified data to the end of the specified
      * zone (using the specified file for the rendering)
      * @return IView
      */
-    public function push( $zone, $file, $datasource = null );    
+    public function push($zone, $file, $datasource = null);
+
     /**
      * Adds the specified data to the top of the specified
      * zone (using the specified file for the rendering)
      * @return IView
      */
-    public function insert( $zone, $file, $datasource = null );       
+    public function insert($zone, $file, $datasource = null);
+
     /**
      * Renders the specified file
      * @return string
      */
-    public function render( $file, $datasource = null );
+    public function render($file, $datasource = null);
+
     /**
      * Renders the current template
      * @return string
      */
-    public function renderTemplate();    
+    public function renderTemplate();
+
     /**
      * Renders the current layout
      * @return string
      */
-    public function renderLayout();    
+    public function renderLayout();
+
     /**
      * Renders the current layout
      * @return string
      */
-    public function renderPlaceholder( $zone );    
+    public function renderPlaceholder($zone);
 }
+
 /**
  * The assets manager structure
  */
-interface IAssets extends IService 
+interface IAssets extends IService
 {
+
     /**
      * Check if the specified package is defined
      * @param string $package 
      * @return boolean
-     */    
-    public function hasConfig( $package );
+     */
+    public function hasConfig($package);
+
     /**
      * Gets the specified package configuration
      * @param string $package 
      * @return array
      * @throws Exception
      */
-    public function getConfig( $package );
+    public function getConfig($package);
+
     /**
      * Attach a package to the current app
      * @param string $package 
      * @return void
      */
-    public function attach( $package );
+    public function attach($package);
+
     /**
      * Remove the package usage
      * @param string $package 
      * @return void
      */
-    public function detach( $package );
+    public function detach($package);
+
     /**
      * Retrieves the list of css includes
      * @return array
      */
     public function getCss();
+
     /**
      * Gets a list of JS links
      * @return array
      */
-    public function getJs();    
+    public function getJs();
 }
+
 /**
  * Services interfaces
  */
-interface IResponse extends IService 
+interface IResponse extends IService
 {
+
     /**
      * Sets the response code
      */
-    function setCode( $code, $message );
+    function setCode($code, $message);
+
     /**
      * Write a new line with the specified message
      */
-    function writeLine( $message );
+    function writeLine($message);
+
     /**
      * Outputs the specified contents
      */
-    function write( $message );
+    function write($message);
 }
+
 /**
  * The logger interface
  */
-interface ILogger extends IService 
+interface ILogger extends IService
 {
     /**
      * Logs debug infos
      */
-    const DEBUG     = 1; // 0001
+    const DEBUG = 1; // 0001
     /**
      * Logs info 
      */
-    const INFO      = 2; // 0010
+    const INFO = 2; // 0010
     /**
      * Logs warnings
      */
-    const WARNING   = 4; // 0100
+    const WARNING = 4; // 0100
     /**
      * Logs errors
      */
-    const ERROR     = 8; // 1000    
+    const ERROR = 8; // 1000    
     /**
      * Gets the current logger level
      * @return int
      */
+
     function getLevel();
+
     /**
      * Sets the log level
      */
-    function setLevel( $level );
+    function setLevel($level);
+
     /**
      * Send a debug message
      */
     function debug($message);
+
     /**
      * Sends an info message
      */
     function info($message);
+
     /**
      * Send a warning message
      */
     function warning($message);
+
     /**
      * Send an error message
      */
     function error($message);
 }
+
 /**
  * The error handler
  */
-interface IErrorHandler 
+interface IErrorHandler
 {
+
     /**
      * Attach an logger and starts watching errors
      */
-    function attach( ILogger $logger );
+    function attach(ILogger $logger);
+
     /**
      * Detach the error handler
      */
     function detach();
+
     /**
      * Catch the specified exception
      */
-    function catchException( \Exception $ex );
+    function catchException(\Exception $ex);
 }
 
 /**
  * Inner service class (automatically loaded)
  */
-class Service extends Event implements IService 
-{    
+class Service extends Event implements IService
+{
     /**
      * on service starts
      */
     const E_LOAD = 'onLoad';
+
     /**
      * Initialize the service
      * @param Application $app 
      */
-    final public function __construct( Application $app ) {
+    final public function __construct(Application $app)
+    {
         parent::__construct($app);
         $this->_onStart();
     }
+
     /**
      * Hook for the service starting
      */
-    protected function _onStart() { 
-        $this->_raise( self::E_LOAD );
+    protected function _onStart()
+    {
+        $this->_raise(self::E_LOAD);
     }
+
     /**
      * Gets the current application
      * @return Application 
      */
-    final public function getApplication() {
+    final public function getApplication()
+    {
         return $this->_app;
     }
+
 }
