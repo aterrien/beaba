3be1aec6b90fd4cb294ac4be6e175e7121a4a71c
Ioan CHIRIAC <i.chiriac@yahoo.com>
2012-08-01 22:39:23 +0200

    start the first plugin :)

---
+++ framework/core/plugins/speed-track/config/plugins.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'speed-track' => array(
+        'enabled' => true,
+        'options' => array(
+            'limit-warn' => 100,
+            'limit-error' => 500
+        )
+    )
+);
