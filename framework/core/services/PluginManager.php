67dbd372ad832c45bed039b7edce42e9ba866a54
Ioan CHIRIAC <i.chiriac@yahoo.com>
2012-08-01 22:38:05 +0200

    add the plugin manager

---
+++ framework/core/services/PluginManager.php
@@ -0,0 +1,12 @@
+<?php
+namespace beaba\core\services;
+use \beaba\core;
+/**
+ * This file is distributed under the MIT Open Source
+ * License. See README.MD for details.
+ * @author Ioan CHIRIAC
+ */
+class PluginManager extends core\Service implements core\IPluginManager 
+{
+
+}
