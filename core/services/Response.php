fb3b051b66f3e279a39c485aea2f49ea6eac7705
Ioan CHIRIAC <i.chiriac@yahoo.com>
2012-08-15 20:42:16 +0200

    add header + fluent mode

--- core/services/Response.php
+++ core/services/Response.php
@@ -1,28 +1,50 @@
 <?php
+
 namespace beaba\core\services;
+
 use \beaba\core;
+
 /**
  * This file is distributed under the MIT Open Source
  * License. See README.MD for details.
  * @author Ioan CHIRIAC
  */
-class Response extends core\Service implements core\IResponse 
-{    
-    public function setCode( $code, $message ) 
+class Response extends core\Service implements core\IResponse
+{
+
+    public function setCode($code, $message)
     {
-        header('HTTP/1.0 '.$code.' '.$message);
-        header('Status: '.$code.' '.$message);
+        header('HTTP/1.0 ' . $code . ' ' . $message);
+        header('Status: ' . $code . ' ' . $message);
+        return $this;
     }
-    public function writeLine( $message ) 
+
+    /**
+     * Sets the response header
+     * @param string $attribute
+     * @param string $value
+     * @return IResponse
+     */
+    public function setHeader($attribute, $value)
     {
-        if ( $this->_app->getRequest()->getResponseType() === 'html' ) {
-            echo $message . '<br />'."\n";
+        header( $attribute . ': ' . $value );
+        return $this;
+    }
+
+    public function writeLine($message)
+    {
+        if ($this->_app->getRequest()->getResponseType() === 'html') {
+            echo $message . '<br />' . "\n";
         } else {
             echo $message . "\n";
-        }        
+        }
+        return $this;
     }
-    public function write( $message ) 
+
+    public function write($message)
     {
         echo $message;
+        return $this;
     }
+
 }
