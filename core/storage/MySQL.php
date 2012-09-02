7d4203b298b028fdb236ed257ab0c4dc1d07eace
Ioan CHIRIAC <i.chiriac@yahoo.com>
2012-09-02 12:09:05 +0200

    finish first impl of the mysql driver

--- core/storage/MySQL.php
+++ core/storage/MySQL.php
@@ -14,7 +14,10 @@ class MySQL implements \beaba\core\IStorageDriver
      * @var array The mysql instance
      */
     protected $_config;
-    
+
+    /**
+     * @var mysqli The SQL client instance
+     */
     protected $_driver;
 
     /**
@@ -25,16 +28,16 @@ class MySQL implements \beaba\core\IStorageDriver
     {
         $this->_config = $config;
         // setting defaults
-        if ( empty($this->_config['host'])) {
+        if (empty($this->_config['host'])) {
             $this->_config['host'] = 'localhost';
         }
-        if ( empty($this->_config['user']) ) {
+        if (empty($this->_config['user'])) {
             $this->_config['user'] = 'root';
         }
-        if ( empty($this->_config['password']) ) {
+        if (empty($this->_config['password'])) {
             $this->_config['password'] = '';
         }
-        if ( empty($this->_config['port']) ) {
+        if (empty($this->_config['port'])) {
             $this->_config['port'] = 3306;
         }
     }
@@ -43,53 +46,139 @@ class MySQL implements \beaba\core\IStorageDriver
      * Gets the MySQL driver
      * @return mysqli
      */
-    protected function _getDriver() {
-        if ( !$this->_driver ) {
+    protected function _getDriver()
+    {
+        if (!$this->_driver) {
             $this->_driver = new \mysqli(
-                $this->_config['host'],
-                $this->_config['user'],
-                $this->_config['password'],
-                empty($this->_config['database']) ?
-                    null : $this->_config['database'],
-                $this->_config['port']
+                    $this->_config['host'],
+                    $this->_config['user'],
+                    $this->_config['password'],
+                    empty($this->_config['database']) ?
+                        null : $this->_config['database'],
+                    $this->_config['port']
             );
         }
         return $this->_driver;
     }
 
-
     /**
-     * Create a select statement
-     * @return IStorageRequest
+     * Handling the exception raising from the driver
+     * @return void
+     * @throws StorageException
      */
-    public function select( \beaba\core\IModel $target ) 
+    protected function _raiseDriverError()
     {
-        return new \beaba\core\StorageRequest(
-            $this, $target
+        throw new \beaba\core\StorageException(
+            'Storage Error ' . $this->_getDriver()->sqlstate . ' : '
+            . $this->_getDriver()->error
         );
     }
 
     /**
-     * Create a select statement
-     * @return IStorageRequest
+     * Escaping a sql entity name
+     * @param string $name
+     * @return string 
      */
-    public function delete( \beaba\core\IModel $target, array $primaries ) {
-        if ( count($primaries) > 1 ) {
-            $this->_getDriver()->query(
-                'DELETE FROM %target% WHERE %id% = %value% LIMIT 1',
-                array(
-                    '$target' => $target->getName(),
-                    '$id' => $target->getPrimary(),
-                    'value' => $primaries[0]
+    protected function _escapeEntity($name)
+    {
+        return '`' . strtr($name, '`\\', '-_') . '`';
+    }
+
+    /**
+     * Escaping a list of entities names
+     * @param array $names 
+     * @return array
+     */
+    protected function _escapeEntities($names)
+    {
+        $result = array();
+        foreach ($names as $name) {
+            $result[] = $this->_escapeEntity($name);
+        }
+        return $result;
+    }
+
+    /**
+     * Escape the specified value
+     * @param string $value
+     * @return string 
+     */
+    protected function _escapeValue($value)
+    {
+        if (is_numeric($value)) {
+            return $value;
+        } elseif (is_null($value)) {
+            return 'null';
+        } elseif (is_string($value)) {
+            return $this->_getDriver()->escape_string($value);
+        } else {
+            throw new \BadMethodCallException(
+                'Could not serialize to SQL the specified value'
+            );
+        }
+    }
+
+    /**
+     * Escape the specified array of values
+     * @param array $values
+     * @return array
+     */
+    protected function _escapeValues(array $values)
+    {
+        $result = array();
+        foreach ($values as $value) {
+            $result[] = $this->_escapeValue($value);
+        }
+        return $result;
+    }
+
+    /**
+     * Executes a SQL statement : UPDATE / INSERT / DELETE
+     * @param string $statement
+     * @return MySQL
+     * @throws StorageException
+     */
+    public function execute($statement)
+    {
+        $result = $this->_getDriver()->query($statement);
+        if ($result === false)
+            $this->_raiseDriverError();
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function select(\beaba\core\IModel $target)
+    {
+        return new \beaba\core\StorageRequest(
+                $this, $target
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function delete(\beaba\core\IModel $target, array $primaries)
+    {
+        $size = count($primaries);
+        $result = true;
+        if ($size === 1) {
+            $this->execute(
+                sprintf(
+                    'DELETE FROM %1$s WHERE %2$s = %3$s LIMIT 1',
+                    $this->_escapeEntity($target->getName()),
+                    $this->_escapeEntity($target->getPrimary()),
+                    $this->_escapeValue($primaries[0])
                 )
             );
-        } else {
-            $this->_getDriver()->query(
-                'DELETE FROM %target% WHERE %id% IN (%values%)',
-                array(
-                    '$target' => $target->getName(),
-                    '$id' => $target->getPrimary(),
-                    'values[]' => $primaries
+        } elseif ($size > 1) {
+            $this->execute(
+                sprintf(
+                    'DELETE FROM %1$s WHERE %2$s IN (%3$s)',
+                    $this->_escapeEntity($target->getName()),
+                    $this->_escapeEntity($target->getPrimary()),
+                    implode(',', $this->_escapeValues($primaries))
                 )
             );
         }
@@ -97,14 +186,43 @@ class MySQL implements \beaba\core\IStorageDriver
     }
 
     /**
-     * Inserts values and returns the created primary
-     * @return integer
+     * @inheritdoc
      */
-    function insert( \beaba\core\IModel $target, array $values );
+    function insert(\beaba\core\IModel $target, array $values)
+    {
+        $this->execute(
+            sprintf(
+                'INSERT INTO %1$s (%2$s) VALUES (%3$s)',
+                $this->_escapeEntity($target->getName()),
+                implode(',', $this->_escapeEntities(array_keys($values))),
+                implode(',', $this->_escapeValues($values))
+            )
+        );
+        return $this->_getDriver()->insert_id;
+    }
 
     /**
-     * Update the specified record with specified values
-     * @return IStorageRequest
+     * @inheritdoc
      */
-    function update( \beaba\core\IModel $target, array $values, $primary );
+    function update(\beaba\core\IModel $target, array $values, $primary)
+    {
+        $pairs = array();
+        foreach ($values as $column => $value) {
+            $pairs[] =
+                $this->_escapeEntity($column)
+                . '='
+                . $this->_escapeValue($value)
+            ;
+        }
+        $this->execute(
+            sprintf(
+                'UPDATE %1$s SET %2$s WHERE %3$s = %4$s LIMIT 1',
+                $this->_escapeEntity($target->getName()), implode(',', $pairs),
+                $this->_escapeEntity($target->getPrimary()),
+                $this->_escapeValue($primary)
+            )
+        );
+        return $this;
+    }
+
 }
