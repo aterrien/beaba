49feb0594acf46cd068045e4c0ef3e2632fb0672
Ioan CHIRIAC <i.chiriac@yahoo.com>
2012-09-18 14:02:29 +0200

    implement the save & delete function + add events over the active record

--- core/Model.php
+++ core/Model.php
@@ -1,11 +1,14 @@
 <?php
-
 namespace beaba\core;
 
 /**
  * This file is distributed under the MIT Open Source
  * License. See README.MD for details.
  * @author Ioan CHIRIAC
+ */
+
+/**
+ * The model representation class (with default implementation)
  */
 class Model implements IModel
 {
@@ -14,7 +17,13 @@ class Model implements IModel
     protected $_app;
     protected $_identifier;
 
-    final public function __construct($identifier, Application $app, $config)
+    /**
+     * Initialize a new model instance
+     * @param string $identifier
+     * @param Application $app
+     * @param array $config 
+     */
+    final public function __construct($identifier, Application $app, array $config)
     {
         $this->_config = $config;
         $this->_app = $app;
@@ -108,7 +117,10 @@ class Model implements IModel
      */
     public function hasColumn($name)
     {
-        return isset($this->_config['columns'][$name]);
+        return 
+            isset($this->_config['columns'][$name])
+            || isset($this->_config['relations'][$name])
+            || $name === $this->getPrimary();
     }
 
     /**
@@ -124,9 +136,53 @@ class Model implements IModel
 /**
  * Defines an active record entity
  */
-class ActiveRecord
+class ActiveRecord extends Event
 {
 
+    /**
+     * The active record is synchronized with the database
+     */
+    const STATE_NONE = 0;
+    /**
+     * The record is new and should be inserted
+     */
+    const STATE_INSERT = 1;
+    /**
+     * The record should be synchronized with the database
+     */
+    const STATE_UPDATE = 2;
+    /**
+     * Before saving the record
+     */
+    const E_BEFORE_SAVE = 'onBeforeSave';
+    /**
+     * Before inserting the record
+     */
+    const E_BEFORE_INSERT = 'onBeforeInsert';
+    /**
+     * Before updating the record
+     */
+    const E_BEFORE_UPDATE = 'onBeforeUpdate';
+    /**
+     * Before deleting the record
+     */
+    const E_BEFORE_DELETE = 'onBeforeDelete';
+    /**
+     * After saving the record
+     */
+    const E_AFTER_SAVE = 'onAfterSave';
+    /**
+     * After inserting the record
+     */
+    const E_AFTER_INSERT = 'onAfterInsert';
+    /**
+     * After updating the record
+     */
+    const E_AFTER_UPDATE = 'onAfterUpdate';
+    /**
+     * After deleting the record
+     */
+    const E_AFTER_DELETE = 'onAfterDelete';
     /**
      * @var array
      */
@@ -138,16 +194,35 @@ class ActiveRecord
     protected $_model;
 
     /**
+     * @var integer The active record state
+     * @see STATE_NONE, STATE_INSERT, STATE_UPDATE
+     */
+    protected $_state;
+    /**
      * Initialize a new record
      * @param IModel $model
      * @param array $data 
      */
-    public function __construct(IModel $model, array $data = null)
-    {
+    final public function __construct(
+        IModel $model, array $data = null, $state = self::STATE_INSERT
+    ) {
         $this->_data = $data ? $data : array();
         $this->_model = $model;
+        $this->_state = $state;
+        parent::__construct( $this->_model->getApplication() );
     }
 
+    /**
+     * Gets the active record primary key value
+     * @return numeric
+     */
+    public function getId() {
+        if ( isset( $this->_data[ $this->_model->getPrimary() ] ) ) {
+            return $this->_data[ $this->_model->getPrimary() ];
+        } else {
+            return null;
+        }
+    }
     /**
      * Gets the specified column value
      * @param string $column
@@ -175,6 +250,9 @@ class ActiveRecord
             throw new \OutOfBoundsException(
                 'Undefined column name : ' . $column
             );
+        }
+        if ( $this->_state === self::STATE_NONE ) {
+            $this->_state = self::STATE_UPDATE;
         }
         $this->_data[$column] = $value;
         return $this;
@@ -208,9 +286,50 @@ class ActiveRecord
      */
     public function save()
     {
-        $this->_model->getStorage()->insert(
-            $this->_model, $this->_data
+        // ignore saving 
+        if ( 
+            $this->_state === self::STATE_NONE 
+        ) return $this;
+        $this->_raise( self::E_BEFORE_SAVE );
+        if ( $this->_state === self::STATE_INSERT ) {
+            $this->_raise(self::E_BEFORE_INSERT);
+            $id = $this->_model->getStorage()->insert(
+                $this->_model, $this->_data
+            );
+            if ( $id !== false ) {
+                $this->_data[ $this->_model->getPrimary() ] = $id;
+                $this->_state = self::STATE_NONE;
+                $this->_raise(self::E_AFTER_INSERT);
+            }
+        } elseif ( $this->_state === self::STATE_UPDATE ) {
+            // handle update
+            $this->_raise(self::E_BEFORE_UPDATE);
+            $this->_model->getStorage()->update(
+                $this->_model, $this->_data, $this->getId()
+            );
+            $this->_state = self::STATE_NONE;
+            $this->_raise(self::E_AFTER_UPDATE);
+        }
+        $this->_raise(self::E_AFTER_SAVE);
+        return $this;
+    }
+    
+    /**
+     * Deleting the current entity
+     * @return ActiveRecord
+     */
+    public function delete() {
+        if ( $this->_state === self::STATE_INSERT ) {
+            return $this;
+        }
+        $this->_raise(self::E_BEFORE_DELETE);
+        // process to deletion
+        $this->_model->getStorage()->delete(
+            $this->_model, array($this->getId())
         );
+        // becomes a new entity (never created in the database)
+        $this->_state = self::STATE_INSERT;
+        $this->_raise(self::E_AFTER_DELETE);
         return $this;
     }
 
